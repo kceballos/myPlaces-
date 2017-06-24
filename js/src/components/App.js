0c146eefbcf03114495538a67899bd3a1a07c1ec
kirsi cebalos <kince0927@gmail.com>
2017-06-24 18:03:58 -0400

    added saved places

--- js/src/components/App.js
+++ js/src/components/App.js
@@ -44,27 +44,29 @@ export default class App extends Component {
     }
 
 
-    render() {
-        // console.log('APP PROPS', this.props);
-        const PLACES = Object.values(this.props.PLACES || {a:0});
-        console.log(PLACES)
+  render() {
+      // console.log('APP PROPS', this.props);
+      const PLACES = Object.values(this.props.PLACES || []);
+      console.log(PLACES)
+      const gmaps = <GMaps {...this.props} apiKey={"AIzaSyBACizJOC_KdP_vmNWSGsHprpJTRTQMfdg"} center={this.state.pos}>
+        <Marker position={this.state.pos} animation="DROP" />
+      </GMaps>
 
-        return (
-        <div>
-
+      return (<div>
         <div>
         	<SidebarRightOverlay handlePlaceClick={this.handlePlaceClick.bind(this)} location={this.state.pos} {...this.props}>
-				{this.state.pos ? <GMaps {...this.props} apiKey={"AIzaSyBACizJOC_KdP_vmNWSGsHprpJTRTQMfdg"} center={this.state.pos}>
-	            	<Marker position={this.state.pos} animation="DROP" />
-	        	</GMaps> : null}
+				    {this.state.pos ? gmaps : null}
         	</SidebarRightOverlay>
-        }
-      }
-
         </div>
+        <div>
+          {PLACES.map(place => {
+            console.log(place)
+            return <h3>{place.name}</h3>
+          })}
         </div>
-        )
-    }
+      
+      </div>)
+  }
 }
 
 //AIzaSyAdHdePExLrWT5do9Y1bdQ72HIlSWpCOG0
